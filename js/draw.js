e87c5124f4509043a383d9af3695da3dd677b01c
Sarah GP <sarah.groff.palermo@gmail.com>
2014-07-11 10:34:52 -0700

    Data update + math works, crashes browswer

--- js/draw.js
+++ js/draw.js
@@ -15,20 +15,47 @@ function draw(){
     }
   }
 
+  function setDiff (possession, index) {
+    var today = new Date(),
+        earliest = new Date(2010, 0, 1),
+        possession = possession,
+        diff = 0;
 
-  var svg = d3.select(".chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+        console.log(possession);
+    
+    if (possession === ''){
+      var max = Date.parse(today);
+      var min = Date.parse(earliest);
+      possession = Math.floor(Math.random() * (max - min) + min)
+    } else {
+      possession = possession.split('-');
+      possession = new Date(possession[0], possession[1], possession[2]);
+      possession = Date.parse(possession); 
+    }
 
-  d3.csv("js/timeline.csv", function(error, data){
+    console.log(possession);
+
+    diff = max - possession;
+
+    return diff;
+
+  }
+
+
+  var svg = d3.select('.chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  d3.csv('js/timeline.csv', function(error, data){
 
     data.forEach(function(element, index){
       data[index].periodical = +data[index].periodical;
-      data[index]['diff'] = Math.floor(Math.random() * (1000 - 5) + 5);
+      data[index]['diff'] = setDiff(element.possession, index);
+      console.log(element.diff);
     }); 
 
-    console.log(data);
+    // console.log(data[1].possession);
 
     svg.selectAll('g')
         .data(data)
